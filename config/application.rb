3743bb79351f7d8e0b56c300ee63963448f5dea0
Marc St Raymond <marc@msaint.com>
2013-02-21 23:17:25 -0800

    added Devise fix for deployment on heroku

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,9 @@ module Selfstarter
     #Crowdtilt API key/secret
     config.crowdtilt_key = "63fccb7868c5f018c167a44d9f6540"
     config.crowdtilt_secret = "76d8019033728a7a8d7222fcf288fefcd13f316c"
+    
+    #Fix for getting Devise to work on Heroku deploy
+    #Forcing app to not access the DB or models when precompiling
+    config.assets.initialize_on_precompile = false
   end
 end

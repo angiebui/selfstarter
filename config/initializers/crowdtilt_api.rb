e8156766183749d3eea7de26a24049bee2497f68
Marc St Raymond <marc@msaint.com>
2013-02-22 09:14:23 -0800

    forcing development environment for Crowdtilt ruby library for now

--- config/initializers/crowdtilt_api.rb
+++ config/initializers/crowdtilt_api.rb
@@ -1,1 +1,3 @@
-Crowdtilt.configure {key Rails.configuration.crowdtilt_key; secret Rails.configuration.crowdtilt_secret; env Rails.env}
+#When ready, we should change the env parameter to Rails.env
+
+Crowdtilt.configure {key Rails.configuration.crowdtilt_key; secret Rails.configuration.crowdtilt_secret; env 'development'}

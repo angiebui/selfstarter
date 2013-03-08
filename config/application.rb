10a6b58ec5bf318a297e562c46f04794e4f10164
Marc St Raymond <marc@msaint.com>
2013-03-07 17:57:03 -0800

    fixing application configuration bug with paperclip

--- config/application.rb
+++ config/application.rb
@@ -36,13 +36,18 @@ module Selfstarter
     config.assets.initialize_on_precompile = false
     
     #Paperclip default options
+    paperclip_path = "/:id/:style.:extension" 
+    if defined?(ENV['CROWDTILT_PRODUCTION_KEY']) && !ENV['CROWDTILT_PRODUCTION_KEY'].nil?
+      paperclip_path = ENV['CROWDTILT_PRODUCTION_KEY'] + paperclip_path
+    end
+
     config.paperclip_defaults = {
       storage: :s3,
       s3_credentials: { 
         access_key_id: ENV['AWS_ACCESS_KEY_ID'], 
         access_key_secret: ENV['AWS_SECRET_ACCESS_KEY']  
       },
-      path: ENV['CROWDTILT_PRODUCTION_KEY'] + "/:id/:style.:extension",
+      path: paperclip_path,
       bucket: "crowdhoster",
       s3_protocol: 'https',
       default_url: '/images/missing_:style.jpg'

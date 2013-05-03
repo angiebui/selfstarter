77ace32f556056337d5e9ecf1fa43595c9d2f7df
Marc St Raymond <marc@msaint.com>
2013-05-03 15:08:27 -0700

    fixed bug with initializing Crowdtilt api client

--- config/initializers/crowdtilt_api.rb
+++ config/initializers/crowdtilt_api.rb
@@ -1,3 +1,12 @@
-Crowdtilt.configure api_key: Rails.configuration.crowdtilt_sandbox_key,
-                    api_secret: Rails.configuration.crowdtilt_sandbox_secret,
-                    mode: Rails.configuration.crowdtilt_mode
+if Rails.configuration.crowdtilt_mode == 'sandbox'
+
+	Crowdtilt.configure api_key: Rails.configuration.crowdtilt_sandbox_key,
+	                    api_secret: Rails.configuration.crowdtilt_sandbox_secret,
+	                    mode: Rails.configuration.crowdtilt_mode
+else
+
+	Crowdtilt.configure api_key: Rails.configuration.crowdtilt_production_key,
+	                    api_secret: Rails.configuration.crowdtilt_production_secret,
+	                    mode: Rails.configuration.crowdtilt_mode
+
+end

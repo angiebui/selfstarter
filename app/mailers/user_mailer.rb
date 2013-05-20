789d409f6a84b9c1befa33a6aaeef934847050ba
Marc St Raymond <marc@msaint.com>
2013-05-20 10:37:31 -0700

    Improving payment emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "no-reply@crowdhoster.com"
+  default from: Rails.configuration.reply_to_email
   
   def payment_confirmation(payment)
     @settings = Settings.find_by_id(1)
     @payment = payment
+    @campaign = @payment.campaign
     mail to: @payment.email, subject: 'You just made a payment!'
   end
 end
